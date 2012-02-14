29bbc66cc403e6d120353015e55823e40c2f2a67
Ben Allen <ben@gurustugroup.com>
2012-02-14 13:56:24 -0600

    added missing mixins scss partial

--- wp/wp-content/themes/mayfest2012/footer.php
+++ wp/wp-content/themes/mayfest2012/footer.php
@@ -15,11 +15,11 @@
 		<footer id="footer" role="contentinfo">
 				<div class="wrap">
 					<?php
-						$fDate = '&copy; 2011';
-						if ( date('Y') != '2011' ) $fDate = $fDate.' - '.date('Y');
+						$fDate = '&copy; 2012';
+						if ( date('Y') != '2012' ) $fDate = $fDate.' - '.date('Y');
 					?>
 					<span class="foot-left"><span>Copyright <?php echo $fDate; ?></span><span class="hyph"> - </span><span><?php bloginfo('name'); ?>. </span><span>All Rights Reserved.</span></span>
-					<span class="foot-right">Site by <a href="http://www.gurustugroup.com" id="guruLink" title="GuRuStu. Branding, Marketing & Web Design." target="_blank">GuRuStu Group</a></span>
+					<span class="foot-right">Site by: <a href="http://www.gurustugroup.com" id="guruLink" title="GuRuStu. Branding, Marketing & Web Design." target="_blank">GuRuStu Group</a></span>
 					<div class="clearfix"></div>
 				</div>
 		</footer><!-- footer -->
